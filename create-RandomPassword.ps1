6e6d34de02034a6f02873551d69f1b1061fd9085
jb-leic <169149943+jb-leic@users.noreply.github.com>
2024-06-28 09:40:33 +0100

    Add files via upload

---
+++ create-RandomPassword.ps1
@@ -0,0 +1,33 @@
+﻿function Shuffle-String([string]$inputString) {
+  $charArray = $inputString.ToCharArray()
+  $rng = New-Object System.Random
+  $remainingChars = $charArray.length
+  while ($remainingChars -gt 1) {
+      $remainingChars--
+      $charIndex = $rng.Next($remainingChars + 1)
+      $value = $charArray[$charIndex]
+      $charArray[$charIndex] = $charArray[$remainingChars]
+      $charArray[$remainingChars] = $value
+  }
+  return -join $charArray
+}
+
+function Create-Password() {
+$TokenSet = @{
+      U = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      L = [Char[]]'abcdefghijklmnopqrstuvwxyz'
+      N = [Char[]]'0123456789'
+      S = [Char[]]'!"#$%&''()*+,-./:;<=>?@[\]^_`{|}~'
+}
+
+$Upper = Get-Random -Count 5 -InputObject $TokenSet.U
+$Lower = Get-Random -Count 5 -InputObject $TokenSet.L
+$Number = Get-Random -Count 5 -InputObject $TokenSet.N
+$Special = Get-Random -Count 5 -InputObject $TokenSet.S
+$Combined = ($Upper + $Lower + $Number + $Special) -join ''
+
+return Shuffle-String $Combined
+}
+
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['password'] = Create-Password
